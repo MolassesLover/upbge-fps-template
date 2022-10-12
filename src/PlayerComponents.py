89da2b45b307df0a8c7230fb085a47da092bf764
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-10-12 11:29:16 +0200

    Reformatted `PlayerComponents.py`

--- src/PlayerComponents.py
+++ src/PlayerComponents.py
@@ -1,27 +1,25 @@
 #   This software is completely free and open-source under the terms of the
-#   GNU General Public License as published by the Free Software Foundation, 
+#   GNU General Public License as published by the Free Software Foundation,
 #   either version 3 of the license, or any later version.
 
-#region Modules
+# region Modules
 
 import bge
 import mathutils
 from collections import OrderedDict
 
-#endregion
+# endregion
+
 
 class PlayerMovement(bge.types.KX_PythonComponent):
-    args = OrderedDict([
-        ("Movement Speed", 0.025),
-        ("Turning Speed", 0.025)
-    ])
+    args = OrderedDict([("Movement Speed", 0.025), ("Turning Speed", 0.025)])
 
-    #region BGE functions
+    # region BGE functions
 
     # start() is called when the script is first loaded.
     def start(self, args):
-        self.movementSpeed = args['Movement Speed']
-        self.turn_speed =args['Turning Speed']
+        self.movementSpeed = args["Movement Speed"]
+        self.turn_speed = args["Turning Speed"]
 
     #  update() is called once per frame.
     def update(self):
@@ -45,4 +43,4 @@ class PlayerMovement(bge.types.KX_PythonComponent):
         direction = mathutils.Vector((horizontal, vertical, 0.0)).normalized()
         self.object.applyMovement((direction.x / 10, direction.y / 10, 0), True)
 
-    #endregion
+    # endregion
